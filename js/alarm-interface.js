507e386b2e97612cc6ec8cf146ab0cfff0ee503d
John Dowd and Jun Fritz <john+jun@>
2017-06-27 14:34:44 -0700

    add functionality for alarm timer.

--- js/alarm-interface.js
+++ js/alarm-interface.js
@@ -1,11 +1,20 @@
 var Clock = require('./../js/alarm.js').clockModule;
 
 $(document).ready(function() {
-  $('#clock').submit(function(event) {
+  $('#clock-form').submit(function(event) {
+    $("#notice").text("");
     event.preventDefault();
     var alarm = $('#time').val();
     var newClock = new Clock(alarm);
-    var output = newClock.SetAlarm();
-    $("#output").append("<li>" + output + "</li>");
+
+    var setTimer = setInterval(timer, 1000);
+      function timer() {
+        console.log(moment().format('H:mm'));
+        if (newClock.SetAlarm(alarm) === true) {
+          console.log("success");
+          $("#notice").text("WAKE UP!");
+          clearInterval(setTimer);
+        }
+      }
   });
 });
--- js/alarm.js
+++ js/alarm.js
@@ -2,14 +2,19 @@ function Clock(Time) {
   this.time = Time;
 }
 
-Clock.prototype.SetAlarm = function() {
-console.log("hello");
+Clock.prototype.SetAlarm = function(alarm) {
+  var status = false;
+  var currentTime = moment().format('HH:mm');
+  console.log(this.time);
+  if (alarm === moment().format('HH:mm')){
+    status = true;
+  }
+  return status;
 };
 
 function update() {
   $('#clock').html(moment().format('MMMM D, YYYY H:mm:ss'));
 }
-
 setInterval(update, 1000);
 
 exports.clockModule = Clock;
